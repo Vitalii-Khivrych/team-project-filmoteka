5dc06fe81efcf12923b8ad7f56c01e4d9483ecc2
NataliaDelicheban <nataliamansarliyska@gmail.com>
2022-07-03 18:41:10 +0200

    add refs, create function to open/close team-modal

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,6 +1,9 @@
 const refs = {
   searchInput: document.querySelector('#search-movie'),
   backdrop: document.querySelector('.backdrop'),
+  openTeamModalBtn: document.querySelector('[data-team-modal-open]'),
+  closeTeamModalBtn: document.querySelector('[data-team-modal-close]'),
+  teamModal: document.querySelector('[data-team-modal]'),
 };
 
 export default refs;
--- src/js/templates/team-modal.js
+++ src/js/templates/team-modal.js
@@ -1,24 +1,18 @@
-// import refs from '../refs';
+import refs from '../refs';
 
-const refs = {
-    openTeamModalBtn: document.querySelector('.footer__button'),
-    closeTeamModalBtn: document.querySelector('.team-modal__btn-close'),
-    modal: document.querySelector('.backdrop__team-modal'),
-  };
+  refs.openTeamModalBtn.addEventListener('click', toggleTeamModal);
+  refs.closeTeamModalBtn.addEventListener('click', toggleTeamModal);
 
-  refs.openTeamModalBtn.addEventListener('click', toggleModal);
-  refs.closeTeamModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+  function toggleTeamModal() {
+    refs.teamModal.classList.toggle('is-hidden');
   };
 
 //   закрытие модалки при нажатии на клавишу Escape
 
-// function modalCloseEsc (event) {
+// function teamModalCloseEsc (event) {
 //     if ( !event.keyCode === 27 ) {
-//         toggleModal();
+//         toggleTeamModal();
 //     }
 //   }
   
-//   document.addEventListener('keydown', modalCloseEsc);
+//   document.addEventListener('keydown', teamModalCloseEsc);
