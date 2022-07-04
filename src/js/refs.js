a1d0a43ca08999c118cbf40e4b50575dfaaec85b
NataliaDelicheban <nataliamansarliyska@gmail.com>
2022-07-05 01:08:23 +0200

    creat functions that creat, render, open/close team-modal

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,9 +1,9 @@
 const refs = {
   searchInput: document.querySelector('#search-movie'),
   backdrop: document.querySelector('.backdrop'),
-  openTeamModalBtn: document.querySelector('[data-team-modal-open]'),
-  closeTeamModalBtn: document.querySelector('[data-team-modal-close]'),
-  teamModal: document.querySelector('[data-team-modal]'),
+  openTeamModalBtn: document.querySelector('.footer__button'),
+  closeTeamModalBtn: document.querySelector('.team-modal__btn-close'),
+  backdropTeamModal: document.querySelector('.backdrop__team-modal'),
 };
 
 export default refs;
