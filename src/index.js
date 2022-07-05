98da220081b585d59485bde9e0ff3c4e761f3aec
NataliaDelicheban <nataliamansarliyska@gmail.com>
2022-07-05 22:30:10 +0200

    Revert "Merge branch 'main' into bug/FT-30"
    This reverts commit 7e0859c9f95b6f65169e73dbff548d66bd43eede, reversing
    changes made to 6e8685349d66e085d573a60878941bb246195559.

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import renderModalCard from './js/templates/render-card-modal';
 import renderPopularCard from './js/templates/render-popular-card';
 import galleryRender from './js/templates/render-gallery';
 import renderFooter from './js/templates/render-footer';
-import Spiner from './js/templates/spiner';
 
-const spiner = new Spiner();
 const appService = new Api();
 
 // -------------Для рендеру карток головної сторінки-----------
--- src/js/firebase.js
+++ src/js/firebase.js
@@ -43,7 +43,6 @@ async function signIn() {
 
 function signOutUser() {
   signOut(getAuth());
-  
 }
 
 function initFirebaseAuth() {
