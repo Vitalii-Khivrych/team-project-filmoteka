a1a4ed3a73d28a8a9ac7b05ac1efe135be368040
dima-zharyy <zharyydimka@gmail.com>
2022-07-06 19:41:20 +0300

    FT-03: left one export localStorageApi

--- src/js/templates/renderQueueList.js
+++ src/js/templates/renderQueueList.js
@@ -6,21 +6,6 @@ const apiService = new Api();
 
 function renderQueueList() {
   const galleryElement = document.querySelector('.gallery');
-
-  // ==================  Раскомментировать для тестов ===================
-  /*
-  localStorageApi.addMovieToQueueList(234); 
-  localStorageApi.addMovieToQueueList(344); 
-  localStorageApi.addMovieToQueueList(123); 
-  localStorageApi.addMovieToQueueList(234); 
-  localStorageApi.addMovieToQueueList(562); 
-  localStorageApi.addMovieToQueueList(184); 
-  localStorageApi.addMovieToQueueList(12); 
-  localStorageApi.addMovieToQueueList(2357); 
-  localStorageApi.addMovieToQueueList(298); 
-  localStorageApi.addMovieToQueueList(5333); 
-  localStorageApi.addMovieToQueueList(5876); 
-*/
   const queueList = localStorageApi.getQueueList();
 
   galleryElement.innerHTML = '';
