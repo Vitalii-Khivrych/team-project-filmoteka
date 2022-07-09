6f42a89ad2421899f2ad355b7fc96b6b9171f063
Yevhen Podoliaka <zanp85740@gmail.com>
2022-07-09 11:18:27 +0300

    fixed modal for trailer

--- src/js/searchTailerById.js
+++ src/js/searchTailerById.js
@@ -1,6 +1,5 @@
 const BASE_URL = 'https://api.themoviedb.org/3';
 const KEY = '81f57ce8a5863d84fdab7c29ba0c37c2';
-import trailerMarkup from './templates/traillerMarkup';
 
 export default async function searchTrailerById(id) {
   const url = `${BASE_URL}/movie/${id}/videos?api_key=${KEY}&language=en-US`;
@@ -10,7 +9,6 @@ export default async function searchTrailerById(id) {
     const trailer = data.results.find(el => el.name.includes('Official'));
     return trailer.key;
   } catch (error) {
-    alert('sorry!!!');
     console.log(error);
   }
 }
--- src/js/templates/traillerMarkup.js
+++ src/js/templates/traillerMarkup.js
@@ -1,16 +1,9 @@
 export default function trailerMarkup(videoKey) {
-  return ` 
+  return `<div
+  class ="modal__video">
     <iframe class="video-frame" src="https://www.youtube.com/embed/${videoKey}"
     frameborder = '0';
     title = 'YouTube video player';
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen></iframe>`;
-}
-//  const videoKey = videos.results[0].key;
-{
-  /* <button class="modal__btn-close-trailer visually-hidden">
-  <svg class="modal__close-icon" width="14" height="14">
-    <use href="${icon}#icon-close"></use>
-  </svg>
-</button>;  */
+      allowfullscreen></iframe></div>`;
 }
