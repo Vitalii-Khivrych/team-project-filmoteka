506e3a8511b7a332484f908535c3383e13a0fc42
Igor Smelyanets <ismelyanets@yahoo.com>
2022-07-10 13:46:38 +0300

    add render Pagination

--- src/js/templates/renderQueueList.js
+++ src/js/templates/renderQueueList.js
@@ -2,24 +2,71 @@ import Api from '../api-service';
 import { renderLibraryCards } from './renderLibraryCards';
 import { localStorageApi } from '../localStorageApi';
 import { addEmptyListPlaceholder } from '../addEmptyListPlaceholder';
+import makePaginatuonBtnMarkup from './paginationMarkup';
+import spiner from '../spiner';
 
-const apiService = new Api();
+const apiServiceLibr = new Api();
 
 function renderQueueList() {
   const queueBtn = document.getElementById('js-queue').classList.add('button-list__btn--current');
     const watchedBtn = document.getElementById('js-watched').classList.remove('button-list__btn--current')
   const galleryElement = document.querySelector('.gallery');
   const queueList = localStorageApi.getQueueList();
-
   galleryElement.innerHTML = '';
 
   if (localStorageApi.isQueueListEmpty()) {
     addEmptyListPlaceholder(galleryElement);
     return;
   }
+  renderFavouritList(queueList, galleryElement, apiServiceLibr);
 
-  const movieCards = queueList.map(id =>
-    apiService.fetchMovieDetails(id).catch(error => console.log(error))
+  const paginationSectionRef = document.querySelector('.pagination');
+  const currentPage = apiServiceLibr.pageNumber;
+  const lastPage = Math.ceil(+queueList.length / 20)
+
+  paginationSectionRef.innerHTML = makePaginatuonBtnMarkup(currentPage, lastPage)
+  
+  const paginationBtn = document.querySelector('.pagination-list ');
+  paginationBtn.addEventListener('click', onPaginationBtnClick);
+
+  function onPaginationBtnClick(e) {
+    e.preventDefault();
+
+    if (+e.target.textContent === currentPage) {
+      console.log('Поточна сторінка');
+      return;
+    }
+
+    if (e.target.id === 'next') {
+      apiServiceLibr.incrementPage();
+
+      renderFavouritList(queueList, galleryElement, apiServiceLibr);
+      paginationSectionRef.innerHTML = makePaginatuonBtnMarkup(apiServiceLibr.pageNumber, lastPage)
+      return;
+    }
+
+    if (e.target.id === 'previous') {
+      apiServiceLibr.decrementPage();
+
+      renderFavouritList(queueList, galleryElement, apiServiceLibr);
+      paginationSectionRef.innerHTML = makePaginatuonBtnMarkup(apiServiceLibr.pageNumber, lastPage)
+      return;
+    }
+
+    if (e.target.textContent !== '...' && e.target.nodeName === 'BUTTON') {
+      // spiner.on();
+      apiServiceLibr.pageNumber = +e.target.textContent;
+
+      renderFavouritList(queueList, galleryElement, apiServiceLibr);
+      paginationSectionRef.innerHTML = makePaginatuonBtnMarkup(apiServiceLibr.pageNumber, lastPage)
+    }
+  }
+}
+function renderFavouritList(queueList, galleryElement, apiServiceEl) {
+  const queuePage = queueList.slice(0+20*(apiServiceEl.pageNumber -1), 20 + 20*(apiServiceEl.pageNumber -1))
+  
+  const movieCards = queuePage.map(id =>
+    apiServiceEl.fetchMovieDetails(id).catch(error => console.log(error))
   );
 
   Promise.all(movieCards)
@@ -28,6 +75,7 @@ function renderQueueList() {
       galleryElement.innerHTML = libraryMarkup;
     })
     .catch(error => console.log(error));
+  
 }
 
-export { renderQueueList };
+export { renderQueueList , renderFavouritList};
--- src/js/templates/renderWatchedList.js
+++ src/js/templates/renderWatchedList.js
@@ -1,9 +1,10 @@
 import Api from '../api-service';
-import { renderLibraryCards } from './renderLibraryCards';
 import { localStorageApi } from '../localStorageApi';
 import { addEmptyListPlaceholder } from '../addEmptyListPlaceholder';
+import { renderFavouritList } from "./renderQueueList";
+import makePaginatuonBtnMarkup from './paginationMarkup';
 
-const apiService = new Api();
+const apiServiceLibrWatched = new Api();
 
 
 function renderWatchedList() {
@@ -19,16 +20,46 @@ function renderWatchedList() {
     return;
   }
 
-  const movieCards = watchedList.map(id =>
-    apiService.fetchMovieDetails(id).catch(error => console.log(error))
-  );
+  renderFavouritList(watchedList, galleryElement, apiServiceLibrWatched);
 
-  Promise.all(movieCards)
-    .then(cards => {
-      const libraryMarkup = renderLibraryCards(cards);
-      galleryElement.innerHTML = libraryMarkup;
-    })
-    .catch(error => console.log(error));
+  const paginationSectionRef = document.querySelector('.pagination');
+  const currentPage = apiServiceLibrWatched.pageNumber;
+  const lastPage = Math.ceil(+watchedList.length / 20)
+
+  paginationSectionRef.innerHTML = makePaginatuonBtnMarkup(currentPage, lastPage)
+  
+  const paginationBtn = document.querySelector('.pagination-list ');
+  paginationBtn.addEventListener('click', onPaginationBtnClick);
+
+  function onPaginationBtnClick(e) {
+    e.preventDefault();
+
+    if (+e.target.textContent === currentPage) {
+      console.log('Поточна сторінка');
+      return;
+    }
+
+    if (e.target.id === 'next') {
+      apiServiceLibrWatched.incrementPage();
+
+      renderFavouritList(watchedList, galleryElement, apiServiceLibrWatched);
+      return;
+    }
+
+    if (e.target.id === 'previous') {
+      apiServiceLibrWatched.decrementPage();
+
+      renderFavouritList(watchedList, galleryElement, apiServiceLibrWatched);
+      return;
+    }
+
+    if (e.target.textContent !== '...' && e.target.nodeName === 'BUTTON') {
+      // spiner.on();
+      apiServiceLibrWatched.pageNumber = +e.target.textContent;
+
+       renderFavouritList(watchedList, galleryElement, apiServiceLibrWatched);
+    }
+  }
 }
 
 export { renderWatchedList };
