5dbc4f21043a133296ed4fdb5da0fb8dfdeb68c5
Igor Smelyanets <ismelyanets@yahoo.com>
2022-07-10 19:56:55 +0300

    add Auth to onLibraryBtn

--- src/js/handlers/onLibraryLinkClick.js
+++ src/js/handlers/onLibraryLinkClick.js
@@ -1,17 +1,20 @@
 import { renderLibraryHeader } from '../templates/header/renderLibraryHeader';
 import { renderQueueList } from '../templates/renderQueueList';
-import { getAuth } from 'firebase/auth';
-import swal from 'sweetalert';
+import { getAuth,  GoogleAuthProvider,
+  signInWithPopup, } from 'firebase/auth';
 import spiner from '../spiner';
 import { changeUrl } from '../service/chengingUrlApi';
 
+
 // Добавить проверку авторизации в начало
 
-const onLibraryLinkClick = event => {
+const onLibraryLinkClick = async(event) => {
   event.preventDefault();
   if (!getAuth().currentUser) {
-    swal('Oops!', 'You need to register to view!', 'warning');
-    return;
+    console.log('Authorized');
+    const provider = new GoogleAuthProvider();
+    await signInWithPopup(getAuth(), provider)
+  
   }
   spiner.on();
 
