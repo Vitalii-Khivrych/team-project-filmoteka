fbc0205b14ae66100250a373c723ac0493b3de41
Vitalii Khivrych <95256813+Vitalii-Khivrych@users.noreply.github.com>
2022-07-11 11:27:19 +0300

    Revert "Feature/FT-53"

--- src/js/handlers/onSearchMovie.js
+++ src/js/handlers/onSearchMovie.js
@@ -11,26 +11,12 @@ const apiServiceSearch = new Api();
 //   searchInput = document.querySelector('#search-movie');
 //   searchInput.addEventListener('submit', onSearchMovie);
 // });
-let currentInputPlaceholder = "";
-const onFocusInput = (event) => {
-  event.preventDefault();
-  if (currentInputPlaceholder.length > 0) {
-    event.currentTarget.placeholder = currentInputPlaceholder;
-    event.currentTarget.classList.add("search-form__input-focus-placeholder");
-  } 
-}
-const onBlurInput = (event) => {
-  event.preventDefault();
-  event.currentTarget.placeholder = "Movie search";
-  event.currentTarget.classList.remove("search-form__input-focus-placeholder");
-}
 
 const onSearchMovie = e => {
   e.preventDefault();
+
   const queryString = e.currentTarget.elements.searchQuery.value.trim();
-  currentInputPlaceholder = queryString;
-  e.currentTarget.elements.searchQuery.placeholder = currentInputPlaceholder;
-  e.currentTarget.elements.searchQuery.classList.add("search-form__input-focus-placeholder");
+
   if (queryString === '') {
     e.target.reset();
     showEmptyInputMessage();
@@ -57,4 +43,4 @@ function searchMovie() {
     .catch(console.log);
 }
 
-export { onSearchMovie, searchMovie,onFocusInput, onBlurInput};
+export { onSearchMovie, searchMovie };
