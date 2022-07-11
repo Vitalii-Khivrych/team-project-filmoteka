5b8b221c525a8b9e3f4fb5da6abfd4b48a95bdeb
Igor Smelyanets <ismelyanets@yahoo.com>
2022-07-12 00:52:03 +0300

    fix delete Watched/Queue movie on Library

--- src/js/handlers/localStorageOptions.js
+++ src/js/handlers/localStorageOptions.js
@@ -1,7 +1,10 @@
 export { onBtnAddToQueueClick, onBtnAddWachedClick };
 import { localStorageApi } from '../localStorageApi';
+import { renderQueueList } from '../templates/renderQueueList';
+import {renderWatchedList} from '../templates/renderWatchedList'
 
 function onBtnAddToQueueClick(e) {
+  const queueStorage = document.getElementById('js-queue');
   if (e.target.dataset.action === 'add-queue') {
     const addToQueue = document.querySelector('.btn-add-queue');
     const addWached = document.querySelector('.btn-add-watched');
@@ -13,12 +16,18 @@ function onBtnAddToQueueClick(e) {
       localStorageApi.addMovieToQueueList(filmId);
       addToQueue.classList.toggle('isActive');
       addToQueue.textContent = 'delete from queue';
+      if (queueStorage.classList.contains('button-list__btn--current')) {
+      renderQueueList()
+    }
       return;
     }
     if (localStorageApi.isMovieInQueueList(filmId)) {
       localStorageApi.removeMovieFromQueueList(filmId);
       addToQueue.classList.toggle('isActive');
       addToQueue.textContent = 'add to queue';
+      if (queueStorage.classList.contains('button-list__btn--current')) {
+      renderQueueList()
+    }
       return;
     }
     if (localStorageApi.isMovieInWatchedList(filmId)) {
@@ -33,6 +42,7 @@ function onBtnAddToQueueClick(e) {
   }
 }
 function onBtnAddWachedClick(e) {
+  const watchedStorage = document.getElementById('js-watched');
   if (e.target.dataset.action === 'add-watched') {
     const addWached = document.querySelector('.btn-add-watched');
     const addToQueue = document.querySelector('.btn-add-queue');
@@ -44,12 +54,18 @@ function onBtnAddWachedClick(e) {
       localStorageApi.addMovieToWatchedList(filmId);
       addWached.classList.toggle('isActive');
       addWached.textContent = 'delete from watched';
+      if (watchedStorage.classList.contains('button-list__btn--current')) {
+        renderWatchedList()
+      }
       return;
     }
     if (localStorageApi.isMovieInWatchedList(filmId)) {
       localStorageApi.removeMovieFromWatchedList(filmId);
       addWached.classList.toggle('isActive');
       addWached.textContent = 'add to watched';
+      if (watchedStorage.classList.contains('button-list__btn--current')) {
+        renderWatchedList()
+      }
       return;
     }
     if (localStorageApi.isMovieInQueueList(filmId)) {
